aeac2357f514db7dd77c045b3e443dcbc0388a5e
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-06-22 13:12:09 +0300

    Update stormcat.rb

--- modules/listener/stormcat.rb
+++ modules/listener/stormcat.rb
@@ -38,6 +38,4 @@ end.parse!
 host = options.local_host
 port = options.local_port
 
-puts "#{g}Using #{host}:#{port}..."
-puts "#{g}Listening on port #{port}..."
 system("ruby core/stormcat.rb -l #{host} -p #{port}")
